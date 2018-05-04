2c39c22b503040a4131be2afc4568345b25d5502
Andy Stewart <lazycat.manatee@gmail.com>
2018-05-04 08:37:22 +0800

    Adjust line number font size.

--- editor/src/texteditor.cpp
+++ editor/src/texteditor.cpp
@@ -1647,7 +1647,7 @@ void TextEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
     int bottom = top + (int) blockBoundingRect(block).height();
     int linenumber = block.blockNumber();
 
-    Utils::setFontSize(painter, document()->defaultFont().pointSize() - 1);
+    Utils::setFontSize(painter, document()->defaultFont().pointSize() - 2);
     while (block.isValid() && top <= event->rect().bottom()) {
         if (block.isVisible() && bottom >= event->rect().top()) {
             if (linenumber + 1 == markStartLine) {
