eecac039f53efefe2aa95ab02a7604d403fdf277
haruyukilxz <lxz@ilxz.me>
2018-07-12 16:17:03 +0800

    update ts
    Change-Id: I97148caa44ac28a22c17a6ff938a560a90668ead

--- translations/deepin-editor.ts
+++ translations/deepin-editor.ts
@@ -2,6 +2,32 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>FindBar</name>
+    <message>
+        <location filename="../editor/src/findbar.cpp" line="37"/>
+        <source>Find: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/findbar.cpp" line="39"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/findbar.cpp" line="40"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JumpLineBar</name>
+    <message>
+        <location filename="../editor/src/jumplinebar.cpp" line="39"/>
+        <source>Go to line: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="../editor/src/main.cpp" line="47"/>
@@ -28,7 +54,231 @@
     </message>
 </context>
 <context>
+    <name>ReplaceBar</name>
+    <message>
+        <location filename="../editor/src/replacebar.cpp" line="37"/>
+        <source>Replace: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/replacebar.cpp" line="39"/>
+        <source>With: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/replacebar.cpp" line="41"/>
+        <source>Replace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/replacebar.cpp" line="42"/>
+        <source>Skip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/replacebar.cpp" line="43"/>
+        <source>Replace Rest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/replacebar.cpp" line="44"/>
+        <source>Replace All</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TextEditor</name>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="83"/>
+        <source>Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="84"/>
+        <source>Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="85"/>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="86"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="87"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="88"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="89"/>
+        <source>Select All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="90"/>
+        <source>Find</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="91"/>
+        <source>Replace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="92"/>
+        <source>Jump line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="93"/>
+        <source>Enable english completer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="94"/>
+        <source>Disable english completer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="95"/>
+        <source>Turn on read only mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="96"/>
+        <source>Turn off read only mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="97"/>
+        <source>Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="98"/>
+        <source>Exit fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="99"/>
+        <source>Open in file manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="100"/>
+        <source>Toggle comment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="101"/>
+        <source>Toggle bullet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="126"/>
+        <source>Convert Case</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="127"/>
+        <source>Upcase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="128"/>
+        <source>Downcase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/texteditor.cpp" line="129"/>
+        <source>Capitalize</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThemeBar</name>
+    <message>
+        <location filename="../editor/src/themebar.cpp" line="56"/>
+        <source>theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Window</name>
+    <message>
+        <location filename="../editor/src/window.cpp" line="131"/>
+        <source>New window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="132"/>
+        <source>New tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="133"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="135"/>
+        <source>Save as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="136"/>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="137"/>
+        <source>Switch theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="138"/>
+        <source>Setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="349"/>
+        <source>Save dragft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="349"/>
+        <source>Do you need to save the draft?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="376"/>
+        <source>Save file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="376"/>
+        <source>Do you need to save content to %1 ?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="391"/>
+        <source>Save root file %1 failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../editor/src/window.cpp" line="491"/>
+        <source>Save File</source>
+        <translation type="unfinished"></translation>
+    </message>
     <message>
         <location filename="../editor/src/window.cpp" line="1216"/>
         <source>Cancel</source>
@@ -40,6 +290,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../editor/src/window.cpp" line="134"/>
         <location filename="../editor/src/window.cpp" line="1218"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
