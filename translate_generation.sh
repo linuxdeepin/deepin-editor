d29ccc06988393467a8dba82c0978493f40ad12d
rekols <rekols@foxmail.com>
2018-11-22 11:11:27 +0800

    feat(desktop): add generic name.
    Change-Id: Ic2db3e07601c85babcb42a062ba0c7757e6991a6

--- translate_generation.sh
+++ translate_generation.sh
@@ -9,3 +9,11 @@ do
     printf "\nprocess ${ts}\n"
     lrelease "${ts}"
 done
+
+# desktop update.
+DESKTOP_TEMP_FILE=deepin-editor.desktop.tmp
+DESKTOP_SOURCE_FILE=deepin-editor.desktop
+DESKTOP_TS_DIR=translations/desktop/
+
+deepin-desktop-ts-convert ts2desktop $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR $DESKTOP_TEMP_FILE
+mv $DESKTOP_TEMP_FILE $DESKTOP_SOURCE_FILE
