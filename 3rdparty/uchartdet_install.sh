06bfea300b184e04f1a5b0844c3b87c9b3f5def8
guoshaoyu <guoshaoyu@uniontech.com>
2021-04-21 14:11:42 +0800

    chore: 修改3rdparty/uchartdet_install.sh;
    Description: 修改3rdparty/uchartdet_install.sh;
    
    Log: 修改3rdparty/uchartdet_install.sh;

--- 3rdparty/uchartdet_install.sh
+++ 3rdparty/uchartdet_install.sh
@@ -2,6 +2,6 @@
 #install iconv lib author liangweidong
 cd uchardet-0.0.7
 path=`pwd`
-cmake . -DCMAKE_INSTALL_PREFIX=$path/../lib
+cmake . --prefix=$path/../lib  -DCMAKE_INSTALL_LIBDIR=lib
 make
 make install
