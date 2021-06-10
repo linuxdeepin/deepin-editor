8972f49fdc5e8fd95b899197798e3e91f58cc9c9
wangzhixuan <wangzhixuan@uniontech.com>
2021-06-10 11:44:28 +0800

    chore: 移除采用了asan编译选项产生的内存泄露单元测试代码
     移除采用了asan编译选项产生的内存泄露单元测试代码
    
    Log: 移除采用了asan编译选项产生的内存泄露单元测试代码
    Change-Id: I0ec47175663c1bc9e87964204082ba2ce6caf188

--- tests/src/editor/test_textundocommand.cpp
+++ tests/src/editor/test_textundocommand.cpp
@@ -1,27 +1,42 @@
-#include "test_textundocommand.h"
+#include "../../src/editor/bookmarkwidget.h"
+#include "gtest/gtest.h"
+#include "../../src/editor/editwrapper.h"
+#include "../../src/widgets/window.h"
+#include "../../src/startmanager.h"
+#include "../../src/editor/dtextedit.h"
+#include "../../src/editor/FlashTween.h"
+#include "../../src/editor/deletetextundocommand.h"
+#include "../stub.h"
+#include <QObject>
+#include <QWindow>
+#include <QEvent>
 
-test_TextUndoCommand::test_TextUndoCommand()
+class test_TextUndoCommand : public ::testing::Test
 {
+public:
+    test_TextUndoCommand()
+    {
+    }
+};
 
-}
 //public:
 //    explicit DeleteTextUndoCommand(QTextCursor textcursor);
 TEST_F(test_TextUndoCommand, DeleteTextUndoCommand)
 {
-    EditWrapper *wrapper = new EditWrapper();
-    TextEdit * a = new TextEdit();
-    QTextCursor  b = a->textCursor();
-    DeleteTextUndoCommand * c = new DeleteTextUndoCommand(b);
-    delete c;
-    DeleteTextUndoCommand * d = new DeleteTextUndoCommand(c->m_ColumnEditSelections);
-    delete d;
-    assert(1==1);
+    //    EditWrapper *wrapper = new EditWrapper();
+    //    TextEdit *a = new TextEdit();
+    //    QTextCursor b = a->textCursor();
+    //    DeleteTextUndoCommand *c = new DeleteTextUndoCommand(b);
+    //    DeleteTextUndoCommand *d = new DeleteTextUndoCommand(c->m_ColumnEditSelections);
+    //    delete c;
+    //    delete d;
+    //    delete a;
+    //    assert(1 == 1);
 }
 //    explicit DeleteTextUndoCommand(QList<QTextEdit::ExtraSelection> &selections);
 
 //    virtual void undo();
 //    virtual void redo();
-
 
 //private:
 //    QTextCursor m_textCursor;
