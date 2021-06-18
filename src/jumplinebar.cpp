7e148836939d6b7052cbc2c87fcf0e5884d114e4
hualei <hualei@uniontech.com>
2021-06-18 15:10:04 +0800

    fix: bug 84266 【1032】【龙芯3A5000】【5.9.0.53】在“跳到行”输入框中可输入大于现有文本行数的数字
    Description: bug 84266 【1032】【龙芯3A5000】【5.9.0.53】在“跳到行”输入框中可输入大于现有文本行数的数字
    
    Log: bug 84266 【1032】【龙芯3A5000】【5.9.0.53】在“跳到行”输入框中可输入大于现有文本行数的数字
    
    link:
    Change-Id: I3067c71fb9cf6c112581587fba2301c22c1c2d3e

--- src/jumplinebar.cpp
+++ src/jumplinebar.cpp
@@ -73,6 +73,7 @@ void JumpLineBar::activeInput(QString file, int row, int column, int lineCount, 
     m_rowBeforeJump = row;
     m_columnBeforeJump = column;
     m_jumpFileScrollOffset = scrollOffset;
+    m_lineCount = lineCount;
     m_lineValidator->setRange(1, lineCount);    
     setFixedSize(nJumpLineBarWidth + QString::number(lineCount).size() * fontMetrics().width('9'),nJumpLineBarHeight);
 
@@ -98,6 +99,10 @@ void JumpLineBar::handleFocusOut()
 void JumpLineBar::handleLineChanged()
 {
     QString content = m_editLine->lineEdit()->text();
+    if (content.toInt() > m_lineCount){
+        m_editLine->lineEdit()->setText("");
+        return;
+    }
     if (content != "") {
         jumpToLine(m_jumpFile, content.toInt(), false);
     }
