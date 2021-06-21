bbaf5b107c9156e8f826902bf6749adf581a3dd7
baohaifeng <baohaifeng@uniontech.com>
2021-06-21 17:19:47 +0800

    fix: 84122 【1032】【龙芯3A5000】【5.9.0.53】新建空白标签页，按Ctrl+J合并行快捷键，响应同按空格键操作
     84122 【1032】【龙芯3A5000】【5.9.0.53】新建空白标签页，按Ctrl+J合并行快捷键，响应同按空格键操作
    
    Log: 84122 【1032】【龙芯3A5000】【5.9.0.53】新建空白标签页，按Ctrl+J合并行快捷键，响应同按空格键操作
    
    Bug: https://pms.uniontech.com/zentao/bug-view-84122.html
    Change-Id: I27fc678d6527beb1b064183768adaa9b77d2b568

--- src/dtextedit.cpp
+++ src/dtextedit.cpp
@@ -1076,6 +1076,8 @@ void TextEdit::cutlines()
 
 void TextEdit::joinLines()
 {
+    if (blockCount() == getCurrentLine())
+        return;
     if (textCursor().hasSelection()) {
         // Get selection bound.
         int startPos = textCursor().anchor();
