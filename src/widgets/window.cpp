7b729640cda52486298a389395c9968500b73d7d
hualei <hualei@uniontech.com>
2022-05-20 16:11:16 +0800

    fix: 取消最小窗口设置
    Description: 取消最小窗口设置
    
    Log: 取消最小窗口设置
    
    link:
    Change-Id: If1c281bbe4488369b5d294579651e5a8a478cdd3

--- src/widgets/window.cpp
+++ src/widgets/window.cpp
@@ -153,7 +153,7 @@ Window::Window(DMainWindow *parent)
     }
 
     // window minimum size.
-    setMinimumSize(680, 300);
+    setMinimumSize(1000, 600);
     // resize window size.
     int window_width = Settings::instance()->settings->option("advance.window.window_width")->value().toInt();
     int window_height = Settings::instance()->settings->option("advance.window.window_height")->value().toInt();
