f261f9d0a0df382a439af78bdd2aef8f58578d01
renbin <renbin@uniontech.com>
2023-07-05 14:24:32 +0800

    fix: Roll back gb18030 midification.
    iconv底层适配GB18030-2022标准,回退上层应用GB18030修改。
    添加DConfig配置是否使用上层修改补丁，当处于2005标准时
    仍会强制使用上层修改补丁以适配2022标准。
    
    Log: 回退GB18030修改
    Influence: GB18030 Encoding

--- src/common/config.h
+++ src/common/config.h
@@ -22,12 +22,17 @@ class Config : public QObject
 public:
     static Config *instance();
     bool enableImproveGB18030() const;
+    bool enablePatchedIconv() const;
+    QByteArray defaultEncoding() const;
 
 private:
 #ifdef DTKCORE_CLASS_DConfigFile
     DConfig *dconfig = nullptr;
 #endif
     bool improveGB18030 = true;  ///< 默认提高GB18030编码识别率
+    bool patchedIconv = false;   ///< 默认不再使用上层修改的iconv
+    bool iocnvUse2005Standard = false;  ///< 检测当前iconv使用的GB18030编码是否为2005标准(2005标准强制使用上层补丁版本)
+    QByteArray encoding;  ///< 缺省编码设置
 };
 
 #endif  // CONFIG_H
