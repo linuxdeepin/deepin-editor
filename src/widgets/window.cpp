819655fc799e5cb1773840f074241d322ce6e91f
Wang Yu <wangyu@uniontech.com>
2024-12-20 16:32:42 +0800

    fix: remove invalid  url
    remove invalid  url
    
    Log: remove invalid  url

--- src/widgets/window.cpp
+++ src/widgets/window.cpp
@@ -2570,7 +2570,7 @@ void Window::addTemFileTab(const QString &qstrPath, const QString &qstrName, con
         return;
     }
 
-    // 若为MIMETYPE不支持的文件，给出无效文件提示 修复bug：https://pms.uniontech.com/bug-view-132653.html
+    // 若为MIMETYPE不支持的文件，给出无效文件提示 修复bug 132653
     if (!Utils::isMimeTypeSupport(qstrPath)) {
         if (currentWrapper() == nullptr) {
             this->addBlankTab();
